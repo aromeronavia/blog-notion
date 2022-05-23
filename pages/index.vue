ec039a4611d852be488af3f2896f6d1e8545ab21
Alberto Romero <aromeronavia@gmail.com>
2022-05-23 14:52:12 -0500

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>yes</div>
+</template>
+
+<script>
+import NotionToHTML from "notion-page-to-html";
+
+export default {
+  created: async function () {
+    const { title, icon, cover, html } = await NotionToHTML.convert(
+      "https://albertoromero.notion.site/The-importance-of-designing-towards-abstractions-e4916a0b0e2d44eca2a9d17985c75c33"
+    );
+
+    console.log({ title, icon, cover, html });
+  },
+};
+</script>
