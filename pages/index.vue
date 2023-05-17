76961009d779047e1f23c92b6ee52c3c6179346a
Alberto Romero <aromeronavia@gmail.com>
2023-05-16 22:57:48 -0600

    Lol

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,90 @@
 <template>
-  <div>yes</div>
+  <div>
+    <NotionRenderer :blockMap="blockMap" fullPage />
+    <h2 class="text-2xl text-center">Leave your comments!</h2>
+    <div class="flex flex-col w-1/2 pl-4 mx-auto">
+      <label for="username">Username:</label>
+      <input v-model="username" class="border" name="username" type="text" />
+      <label for="email">Email:</label>
+      <input v-model="email" class="border" name="email" type="text" />
+      <label for="email">Comment:</label>
+      <textarea
+        v-model="comment"
+        class="border"
+        name="comment"
+        cols="3"
+        rows="3"
+        maxlength="1000"
+      />
+      <button @click="submitComment" class="rounded bg-blue text-md border p-3">
+        Submit
+      </button>
+    </div>
+    <div v-for="comment in comments" :key="comment.id">
+      <p>{{ comment.username }}</p>
+      <p>{{ comment.email }}</p>
+      <p>{{ comment.comment }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import NotionToHTML from "notion-page-to-html";
+const POST_ID = "13de327a4d534d4889d3dbc0e805afbd"
 
 export default {
-  created: async function () {
-    const { title, icon, cover, html } = await NotionToHTML.convert(
-      "https://albertoromero.notion.site/The-importance-of-designing-towards-abstractions-e4916a0b0e2d44eca2a9d17985c75c33"
-    );
+  data: () => ({
+    blockMap: null,
+    comments: [],
+    username: "",
+    comment: "",
+    email: "",
+  }),
+  methods: {
+    async submitComment() {
+      try {
+        await this.$supabase.from("comment").insert({
+          post_id: POST_ID,
+          username: this.username,
+          email: this.email,
+          comment: this.comment,
+        });
+      } catch (error) {
+        console.error(error);
+      }
 
-    console.log({ title, icon, cover, html });
+      this.username = "";
+      this.email = "";
+      this.comment = "";
+    },
+    async getComments() {
+      const comments = await this.$supabase.from("comment").insert({
+        post_id: POST_ID,
+        username: this.username,
+        email: this.email,
+        comment: this.comment,
+      });
+
+      this.comments = comments;
+    },
+  },
+  async asyncData({ $notion }) {
+    // use Notion module to get Notion blocks from the API via a Notion pageId
+    const blockMap = await $notion.getPageBlocks(
+      POST_ID,
+    );
+    return { blockMap };
+  },
+  async mounted() {
+    const comments = await this.$supabase
+      .from("comment")
+      .select()
+      .eq("post_id", POST_ID);
+
+    this.comments = comments.body;
   },
 };
 </script>
+
+<style>
+@import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+</style>
